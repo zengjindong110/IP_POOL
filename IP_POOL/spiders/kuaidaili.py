79a5815b4736a40d6aad237e1a8ab2427fabae9c
jindong <248381089@qq.com>
2021-06-05 20:00:58 +0800

    恶气呃

---
+++ IP_POOL/spiders/kuaidaili.py
@@ -0,0 +1,25 @@
+import scrapy
+from scrapy import cmdline, Spider
+from IP_POOL.items import KuaidailiItem
+
+
+class KuaidailiSpider(scrapy.Spider):
+    name = 'kuaidaili'
+    allowed_domains = ['www.kuaidiali.com']
+    start_urls = ['https://www.kuaidaili.com/free/inha/1/']
+
+    def parse(self, response):
+        ips = response.xpath("//table[@class='table table-bordered table-striped']/tbody/tr")
+        item = KuaidailiItem()
+
+        for ip in ips:
+            item["ip"] = ip.xpath("./td[1]/text()").get()
+            item["port"] = ip.xpath("./td[2]/text()").get()
+            item["type"] = ip.xpath("./td[4]/text()").get()
+            item["platform"] = self.name
+
+            yield item
+
+
+if __name__ == '__main__':
+    cmdline.execute("scrapy crawl kuaidaili --nolog".split())
